b494f2c39ea76802d6a49b900efd27000aebbc0c
Tacticacti <edwardoh@tudelft.nl>
2025-12-03 01:30:59 +0100

    Added asteroids functionalities

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,12 +11,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = {updatable, drawable}
+    Asteroid.containers = {asteroids, updatable, drawable}
+    AsteroidField.containers = {updatable}
+
     
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
